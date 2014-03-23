bfa4f8d8a35bb5ace91c877e685f3b87f77feb81
Sean <toranos@gmail.com>
2014-03-23 23:49:20 +1100

    Reversi Code Revision
    Rather than use a bitmap for the board, now renders it at runtime using
    an array of Pygame rects. This also allows for easier click detection
    using point collision with the rects.

--- reversi.py
+++ reversi.py
@@ -2,27 +2,58 @@ import pygame
 pygame.init()
 
 size = width, height = 1280, 800 # screen size
-black = 0, 0, 0
+background = 255, 255, 255
 
 screen = pygame.display.set_mode(size)
 
-speed = [1,1]
+space = pygame.Surface((70,70))
+space.fill((0,0,0))
 
-board_reversi = pygame.image.load("board_reversi.png").convert()
-board_reversi_rect = board_reversi.get_rect()
+spaces = [[0 for x in range(8)] for x in range(8)] #array init
+for x in range(8):
+    for y in range(8):
+        spaces[x][y] = pygame.Rect((135 + 80 * x, 85 + 80 * y),(70,70))
+"""
+spaces[0][0] = ((135,85),(70,70))
+spaces[1][0] = ((215,85),(70,70))
+spaces[2][0] = ((295,85),(70,70))
+spaces[3][0] = ((375,85),(70,70))
+spaces[4][0] = ((455,85),(70,70))
+spaces[5][0] = ((535,85),(70,70))
+spaces[6][0] = ((615,85),(70,70))
+spaces[7][0] = ((695,85),(70,70))"""
+
+#board_reversi = pygame.image.load("board_reversi.png").convert()
+#board_reversi_rect = ((100,50),board_reversi.get_size())
 
 while 1: #infinite loop
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # mouse click coordinates in x,y
+            click_pos = event.pos
+            for x in range(8):
+                for y in range(8):
+                    if spaces[x][y].collidepoint(click_pos):
+                        #pass x,y into validMoveCheck
+                        print("ninjas", x, y) #just a thing to close the if block
 
-    board_reversi_rect = board_reversi_rect.move(speed)
-    if board_reversi_rect.left < 0 or board_reversi_rect.right > width:
-        speed[0] = -speed[0]
-    if board_reversi_rect.top < 0 or board_reversi_rect.bottom > height:
-        speed[1] = -speed[1]
+    screen.fill(background)
+    #screen.blit(board_reversi, board_reversi_rect)
+    """
+    screen.blit(space, spaces[0][0])
+    screen.blit(space, spaces[1][0])
+    screen.blit(space, spaces[2][0])
+    screen.blit(space, spaces[3][0])
+    screen.blit(space, spaces[4][0])
+    screen.blit(space, spaces[5][0])
+    screen.blit(space, spaces[6][0])
+    screen.blit(space, spaces[7][0])"""
 
-    screen.fill(black)
-    screen.blit(board_reversi, board_reversi_rect)
+    for x in range(8):
+        for y in range(8):
+                screen.blit(space, spaces[x][y])
+
     pygame.display.flip()
-
-    pygame.time.delay(10) #delays by 1/100 of a second to slow down movement
+    #pygame.time.delay(10) #delays by 1/100 of a second to slow down movement
