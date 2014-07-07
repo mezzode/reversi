577a0f18bd329f65b4871f4d7f98fc514c4dc5e2
Sean <toranos@gmail.com>
2014-07-07 17:57:22 +1000

    Updated setup.py
    Better setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,22 @@ base = None
 if (sys.platform == "win32"):
     base = "Win32GUI"    # Tells the build script to hide the console.
 
+options = {
+    'build_exe': {
+        'include_msvcr': True,
+        'icon': 'favicon.ico',
+        # 'include_files': sys.path + ['Quicksand-Light.ttf']
+        'include_files': ['Quicksand-Light.ttf']
+    }
+}
+
+executables = [
+    Executable("main.pyw",base=base)
+]
+
 setup(  name = "reversi2014",
         version = "0.1.0",
         description = "Reversi 2014 Alpha Build",
-        # options = {"build_exe": build_exe_options},
-        executables = [Executable("main.pyw", base=base)])
+        options = options,
+        executables = executables
+        )
