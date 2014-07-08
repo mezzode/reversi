e71f6ad9ecc8f0ab8d1a2331c4cad88afc6ed25b
Sean <toranos@gmail.com>
2014-07-08 17:46:43 +1000

    Reversi Readability (Sections and Comments)
    Sectioned, re-ordered and added comments to the Reversi code to increase
    Readability.

--- reversi.py
+++ reversi.py
@@ -5,32 +5,37 @@ class game:
     'A class for games of Reversi'
 
     def __init__(self):
+        # Game init
         self.turn = 1
         self.space_states = [[0 for x in range(8)] for x in range(8)]
         self.space_states[3][4] = 1
         self.space_states[4][3] = 1
         self.space_states[3][3] = 2
         self.space_states[4][4] = 2
-        # 0 = empty, 1 = player 1 (black), 2 = player 2 (white)
-        self.info = "Dark plays first"
-        self.infoPerm = "Dark plays first"
-        # self.infoTemp = ""
+        # Key:
+        # 0 = empty
+        # 1 = P1 (i.e.  dark)
+        # 2 = P2 (i.e. light)
+        self.info = "Dark plays first" # Currently displayed info
+        self.infoPerm = "Dark plays first" # Persistent info
         self.p1_help_on = False
         self.p2_help_on = False
+        self.consecutive_skips = 0
 
     def nextTurn(self):
         self.turn += 1
 
     def updateInfo(self, newText):
-        # self.old_info = self.info
-        # self.info = newText
+        # Update infobox with permanent newText
         self.infoPerm = newText
         self.info = self.infoPerm
 
     def hoverInfo(self,newText):
+        # Update infobox with temporary newText
         self.info = newText
 
     def resetInfo(self):
+        # Reset infobox to permanent text
         self.info = self.infoPerm
 
     def helpToggle(self):
@@ -38,16 +43,20 @@ class game:
             if self.p1_help_on == True:
                 self.p1_help_on = False
                 self.updateInfo("Help is off for P1")
+                self.hoverInfo("Show moves")
             elif self.p1_help_on == False:
                 self.p1_help_on = True
                 self.updateInfo("Help is on for P1")
+                self.hoverInfo("Hide moves")
         else:
             if self.p2_help_on == True:
                 self.p2_help_on = False
                 self.updateInfo("Help is off for P2")
+                self.hoverInfo("Show moves")
             elif self.p2_help_on == False:
                 self.p2_help_on = True
                 self.updateInfo("Help is on for P2")
+                self.hoverInfo("Hide moves")
 
     def player(self):
         if self.turn % 2 == 1: # if turn is odd
@@ -70,26 +79,16 @@ class game:
             help_on = self.p2_help_on
         return help_on
 
-# if one player has no valid moves, play passes to the second player
-
-# def currentPlayer(r0):
-#     if r0.turn % 2 == 1: # if turn is odd
-#         player = 1
-#         enemy  = 2
-#     else:
-#         player = 2
-#         enemy  = 1
-#     return (player, enemy)
-
 def clickCheck (click_pos, mode, spaces,r0):
+    # Determines what should be done in response to a mouse click
+
+    # if collides with a space
     for x in range(8):
         for y in range(8):
             if spaces[x][y].collidepoint(click_pos):
-                # pass x,y into moveCheck
                 moveCheck(x,y,r0)
-                # debugging:
-                # print("clicked:", x, y)
 
+    # Template:
     # if collides with buttons or other clickable things:
         # do stuff
     if button_forfeit_rect.collidepoint(click_pos):
@@ -100,29 +99,20 @@ def clickCheck (click_pos, mode, spaces,r0):
                 mode[m] = False
         button_forfeit_surface.fill(panel_colour)
 
-    if button_help_rect.collidepoint(click_pos): # if collides with help button
-        # toggle
-        # change toggling to a method?
-
+    if button_help_rect.collidepoint(click_pos):
+        # if collides with help button
         r0.helpToggle()
-        # r0.updateInfo("Help is on")
-        # button_help_surface.fill((200,200,200))
-        # if r0.help_on == True:
-        #     r0.help_on = False
-        #     button_help_surface.fill(panel_colour)
-        # elif r0.help_on == False:
-        #     r0.help_on = True
-        #     button_help_surface.fill((50,50,50))
+
+    # Colours help button accordingly
     if r0.help_on() == False:
         button_help_surface.fill(panel_colour)
     elif r0.help_on() == True:
-        button_help_surface.fill((230,230,230))
-        # button_help_surface.fill((200,200,200))
-        # button_help_surface.fill(helpGreen)
+        button_help_surface.fill(light)
+
     return
 
 def spaceCheck (x,y,r0, to_flip, flip_buffer):
-    # player, enemy = currentPlayer(r0)
+    # Checks if a space should be flipped
     if r0.space_states[x][y] == r0.enemy():
         flip_buffer.append((x,y))
         done = False
@@ -136,34 +126,39 @@ def spaceCheck (x,y,r0, to_flip, flip_buffer):
     return done
 
 def moveMaker (r0, to_flip):
+    # Makes a move i.e. places and flips pieces
+
+    # Flips pieces 
     count = -1
     for xy in to_flip:
         print(str(xy)) # from stub
         x, y = xy
         r0.space_states[x][y] = r0.player()
         count += 1
-    # r0.updateInfo("P" + str(player) + " took " + str(count) + " of " + "P" + str(enemy))
-    # change wording?
-    # r0.updateInfo("P"+str(player)+" captured "+str(count))
+
+    # Displays how many pieces were flipped
     if count > 1:
         r0.updateInfo("P"+str(r0.player())+" took "+str(count)+" pieces")
     else:
         r0.updateInfo("P"+str(r0.player())+" took "+str(count)+" piece")
     r0.nextTurn()
+
     return
 
 def moveCheck (x,y,r0):
+    # Checks whether a move is valid
+
     xy = x, y
     to_flip = []
     flip_buffer = []
 
-    if r0.space_states[x][y] == 0: #empty
+    if r0.space_states[x][y] == 0: # empty
         valid_move = True
     else:
         valid_move = False
     
     if valid_move:
-        #check north
+        # check north
         done = False
         x, y = xy
         while y > 0 and done == False:
@@ -171,7 +166,7 @@ def moveCheck (x,y,r0):
             done = spaceCheck (x,y,r0, to_flip, flip_buffer)
         flip_buffer = []
         
-        #check south
+        # check south
         done = False
         x, y = xy
         while y < 7 and done == False:
@@ -179,7 +174,7 @@ def moveCheck (x,y,r0):
             done = spaceCheck (x,y,r0, to_flip, flip_buffer)
         flip_buffer = []
         
-        #check east
+        # check east
         done = False
         x, y = xy
         while x < 7 and done == False:
@@ -187,7 +182,7 @@ def moveCheck (x,y,r0):
             done = spaceCheck (x,y,r0, to_flip, flip_buffer)
         flip_buffer = []
         
-        #check west
+        # check west
         done = False
         x, y = xy
         while x > 0 and done == False:
@@ -195,7 +190,7 @@ def moveCheck (x,y,r0):
             done = spaceCheck (x,y,r0, to_flip, flip_buffer)
         flip_buffer = []
         
-        #check NE
+        # check NE
         done = False
         x, y = xy
         while y > 0 and x < 7 and done == False:
@@ -204,7 +199,7 @@ def moveCheck (x,y,r0):
             done = spaceCheck (x,y,r0, to_flip, flip_buffer)
         flip_buffer = []
         
-        #check SE
+        # check SE
         done = False
         x, y = xy
         while y < 7 and x < 7 and done == False:
@@ -213,7 +208,7 @@ def moveCheck (x,y,r0):
             done = spaceCheck (x,y,r0, to_flip, flip_buffer)
         flip_buffer = []
         
-        #check SW
+        # check SW
         done = False
         x, y = xy
         while y < 7 and x > 0 and done == False:
@@ -222,7 +217,7 @@ def moveCheck (x,y,r0):
             done = spaceCheck (x,y,r0, to_flip, flip_buffer)
         flip_buffer = []
         
-        #check NW
+        # check NW
         done = False
         x, y = xy
         while y > 0 and x > 0 and done == False:
@@ -242,16 +237,20 @@ def moveCheck (x,y,r0):
     return
 
 def mouseCheck (mouse_pos,r0):
-    # if collides with buttons or other clickable things:
+    # Determines what should be done in response to a mouse movement
+
+    # Template:
+    # if collides with something:
         # do stuff
     if button_forfeit_rect.collidepoint(mouse_pos):
+        # collides with forfeit button
         print("Highlight Forfeit") # from stub
         button_forfeit_surface.fill((200,80,80))
         r0.hoverInfo("Quits to menu")
-    elif button_help_rect.collidepoint(mouse_pos): # if collides with help button
+    elif button_help_rect.collidepoint(mouse_pos):
+        # collides with help button
         print("Highlight Help") # from stub
-        # button_help_surface.fill((230,230,230))
-        button_help_surface.fill((240,240,240))
+        button_help_surface.fill(highlight_colour)
         if r0.help_on():
             r0.hoverInfo("Hide moves")
         else:
@@ -262,12 +261,11 @@ def mouseCheck (mouse_pos,r0):
         if r0.help_on() == False:
             button_help_surface.fill(panel_colour)
         elif r0.help_on() == True:
-            button_help_surface.fill((230,230,230))
-            # button_help_surface.fill((200,200,200))
-            # button_help_surface.fill(helpGreen)
+            button_help_surface.fill(light)
     return
 
 def helpCheck (r0):
+    # Determines current player's valid moves
     for x0 in range(8):
         for y0 in range(8):
             space_help[x0][y0] = 0
@@ -281,7 +279,7 @@ def helpCheck (r0):
                 valid_move = False
             
             if valid_move:
-                #check north
+                # check north
                 done = False
                 x, y = xy
                 while y > 0 and done == False:
@@ -289,7 +287,7 @@ def helpCheck (r0):
                     done = spaceCheck (x,y,r0, to_flip, flip_buffer)
                 flip_buffer = []
                 
-                #check south
+                # check south
                 done = False
                 x, y = xy
                 while y < 7 and done == False:
@@ -297,7 +295,7 @@ def helpCheck (r0):
                     done = spaceCheck (x,y,r0, to_flip, flip_buffer)
                 flip_buffer = []
                 
-                #check east
+                # check east
                 done = False
                 x, y = xy
                 while x < 7 and done == False:
@@ -305,7 +303,7 @@ def helpCheck (r0):
                     done = spaceCheck (x,y,r0, to_flip, flip_buffer)
                 flip_buffer = []
                 
-                #check west
+                # check west
                 done = False
                 x, y = xy
                 while x > 0 and done == False:
@@ -313,7 +311,7 @@ def helpCheck (r0):
                     done = spaceCheck (x,y,r0, to_flip, flip_buffer)
                 flip_buffer = []
                 
-                #check NE
+                # check NE
                 done = False
                 x, y = xy
                 while y > 0 and x < 7 and done == False:
@@ -322,7 +320,7 @@ def helpCheck (r0):
                     done = spaceCheck (x,y,r0, to_flip, flip_buffer)
                 flip_buffer = []
                 
-                #check SE
+                # check SE
                 done = False
                 x, y = xy
                 while y < 7 and x < 7 and done == False:
@@ -331,7 +329,7 @@ def helpCheck (r0):
                     done = spaceCheck (x,y,r0, to_flip, flip_buffer)
                 flip_buffer = []
                 
-                #check SW
+                # check SW
                 done = False
                 x, y = xy
                 while y < 7 and x > 0 and done == False:
@@ -340,7 +338,7 @@ def helpCheck (r0):
                     done = spaceCheck (x,y,r0, to_flip, flip_buffer)
                 flip_buffer = []
                 
-                #check NW
+                # check NW
                 done = False
                 x, y = xy
                 while y > 0 and x > 0 and done == False:
@@ -353,19 +351,20 @@ def helpCheck (r0):
                     valid_move = False
 
                 if valid_move:
-                    # to_flip.append(xy)
-                    # turn = moveMaker(space_states, to_flip, turn)
                     space_help[x0][y0] = 1
     return
 
 def boardRender (screen, r0):
+    # Updates non-permanent display elements
+
+    # Display board spaces
     for x in range(8):
         for y in range(8):
             screen.blit(space, spaces[x][y])
 
+    # Count pieces and empty spaces
     score_p1 = 0
     score_p2 = 0
-
     empty_space_count = 0
     for x in range(8):
         for y in range(8):
@@ -378,21 +377,22 @@ def boardRender (screen, r0):
             elif r0.space_states[x][y] == 0:
                 empty_space_count += 1
 
-    # player, enemy = currentPlayer(r0)
-
-    if empty_space_count == 0:
-        # game end
+    # Determine whether game has ended
+    if empty_space_count == 0 or r0.consecutive_skips == 2:
+        # Game has ended
         if score_p1 > score_p2:
-            # end = "Player 1 Wins!"
+            print("Player 1 Wins!") # from stub
             r0.updateInfo("Player 1 Wins!")
         elif score_p1 < score_p2:
-            # end = "Player 2 Wins!"
+            print("Player 2 Wins!") # from stub
             r0.updateInfo("Player 2 Wins!")
         elif score_p1 == score_p2:
-            # end = "You both won!"
+            print("You both won!") # from stub
             r0.updateInfo("You both won!")
-
     else:
+        # Game has not ended
+
+        # Determine valid moves, highlight if help is on
         helpCheck(r0)
         valid_move_count = 0
         for x in range(8):
@@ -402,28 +402,30 @@ def boardRender (screen, r0):
                     if r0.help_on():
                         screen.blit(help_counter, spaces[x][y])
         if valid_move_count == 0:
-            r0.updateInfo("No valid move for P" + str(r0.player()))
+            # Current player has no valid moves, skips turn
+            print("No valid move") # from stub
+            r0.updateInfo("No moves for P" + str(r0.player()))
             r0.nextTurn()
+            r0.consecutive_skips += 1
+        else:
+            # Current player has valid moves
+            r0.consecutive_skips = 0
 
-    if r0.player() == 2:
-        panel_move_surface.fill(light) # now it is lights move
-        label_move = font_med.render("P2's Move",1,(0,0,0))
-    elif r0.player() == 1:
-        panel_move_surface.fill(dark) # now it is darks move
-        label_move = font_med.render("P1's Move",1,(255,255,255))
+    # Set non-permanent labels
+    label_dark = font_med.render(("P1 - "+str(score_p1)),1,white)
+    label_light = font_med.render(("P2 - "+str(score_p2)),1,black)
+    label_turn = font_small.render(("Turn " + str(r0.turn)),1,black)
+    label_info = font_small.render(r0.info,1,black)
+    if r0.player() == 1:
+        # Dark's (i.e. P1's) Move
+        panel_move_surface.fill(dark)
+        label_move = font_med.render("P1's Move",1,white)
+    elif r0.player() == 2:
+        # Light's (i.e. P2's) Move
+        panel_move_surface.fill(light)
+        label_move = font_med.render("P2's Move",1,black)
 
-    # if r0.help_on == False:
-    #     button_help_surface.fill(panel_colour)
-    # elif r0.help_on == True:
-    #     button_help_surface.fill((50,50,50))
-
-    label_dark = font_med.render(("P1 - "+str(score_p1)),1,(255,255,255))
-    label_light = font_med.render(("P2 - "+str(score_p2)),1,(0,0,0))
-    label_turn = font_small.render(("Turn " + str(r0.turn)),1,(0,0,0))
-
-    label_info = font_small.render(r0.info,1,(0,0,0))
-
-    # print panels to screen
+    # Display panels to screen
     screen.blit(panel_move_surface, panel_move_rect)
     screen.blit(panel_dark_surface, panel_dark_rect)
     screen.blit(panel_light_surface, panel_light_rect)
@@ -433,7 +435,7 @@ def boardRender (screen, r0):
     screen.blit(button_help_surface,button_help_rect)
     screen.blit(panel_info_surface,panel_info_rect)
 
-    # create label rects
+    # Create label rects
     label_move_rect = label_move.get_rect()
     label_dark_rect = label_dark.get_rect()
     label_light_rect = label_light.get_rect()
@@ -443,7 +445,7 @@ def boardRender (screen, r0):
     label_help_rect = label_help.get_rect()
     label_info_rect = label_info.get_rect()
 
-    # center labels on panels
+    # Centre labels on panels
     label_move_rect.center = panel_move_rect.center
     label_dark_rect.center = panel_dark_rect.center
     label_light_rect.center = panel_light_rect.center
@@ -453,16 +455,7 @@ def boardRender (screen, r0):
     label_help_rect.center = button_help_rect.center
     label_info_rect.center = panel_info_rect.center
 
-    # screen.blit(label_move,panel_move_rect)
-    # screen.blit(label_dark,panel_dark_rect)
-    # screen.blit(label_light,panel_light_rect)
-    # screen.blit(label_turn,panel_turn_rect)
-    # screen.blit(label_forfeit,button_forfeit_rect)
-    # screen.blit(label_rules,button_rules_rect)
-    # screen.blit(label_help,button_help_rect)
-    # screen.blit(label_info,panel_info_rect)
-
-    # print labels to screen
+    # Display labels to screen
     screen.blit(label_move,label_move_rect)
     screen.blit(label_dark,label_dark_rect)
     screen.blit(label_light,label_light_rect)
@@ -474,19 +467,28 @@ def boardRender (screen, r0):
 
     return
 
+#############################
+# Permanent Screen Elements #
+#############################
+
+# Colours
+black = 0,0,0
+white = 255,255,255
+dark  = 0,0,0                  # black
+light = 230, 230, 230          # light grey
+panel_colour = 253,253,253     # off white
+space_colour = 153, 204, 153   # light green
+helpGreen = 100,200,100        # brighter green
+highlight_colour = 240,240,240 # lighter grey
+
+# Fonts
 font_small = pygame.font.Font("Quicksand-Light.ttf", 44)
 font_med = pygame.font.Font("Quicksand-Light.ttf", 48)
 
+# Labels
 label_forfeit = font_small.render("Forfeit",1,(0,0,0))
 label_rules = font_small.render("Rules",1,(0,0,0))
 label_help = font_small.render("Help",1,(0,0,0))
-
-# Colours
-dark  = 0,0,0                # black
-light = 230, 230, 230        # light grey
-panel_colour = 253,253,253   # off white
-space_colour = 153, 204, 153 # light green
-helpGreen = 100,200,100      # brighter green
 
 # P1/P2's Move Panel
 panel_move_rect = pygame.Rect((785,85),(360,100))
