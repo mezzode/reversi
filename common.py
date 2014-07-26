844310e1ec59b5d8e8fba3fd696a5ddc1396b87a
Sean <toranos@gmail.com>
2014-07-26 15:28:53 +1000

    Responses to Invalid Configs
    Changed code so that invalid config settings will cause colours to
    default.

--- common.py
+++ common.py
@@ -8,7 +8,7 @@ config = configparser.ConfigParser()
 # config['DEFAULT'] = {} # default values for each section here
 config['colours'] = {}
 colours = config['colours']
-# colours['dark'] = '0,0,0'
+colours['dark'] = '0,0,0'
 colours['light'] = '230,230,230'
 colours['space_colour'] = '153, 204, 153'
 colours['help_colour']  = '100,200,100'
@@ -22,12 +22,23 @@ window['height'] = '800'
 with open('config.ini','w') as configfile: # save to config.ini
     config.write(configfile)
 
-def strToColour(colourStr):
-    r,g,b = colourStr.split(',')
-    r = int(r)
-    g = int(g)
-    b = int(b)
-    colour = r,g,b
+def colourCheck(colour,defaultColour):
+    if isinstance(colour,str):
+        try:
+            r,g,b = colour.split(',')
+            r = int(r)
+            g = int(g)
+            b = int(b)
+            if r > 255 or g > 255 or b > 255:
+                colour = defaultColour
+            else:
+                colour = r,g,b
+        except ValueError:
+            colour = defaultColour
+    elif isinstance(colour,tuple):
+        colour = colour
+    else:
+        colour = defaultColour
     return colour
 
 # Colours
@@ -37,12 +48,14 @@ red = 255,100,100
 purple = 255,0,255
 blue = 0,0,100
 yellow = 200,200,0
-# dark  = 0,0,0                  # black
-# light = 230, 230, 230          # light grey
-# panel_colour = 253,253,253     # off white
-# space_colour = 153, 204, 153   # light green
-# helpGreen = 100,200,100        # brighter green
-# highlight_colour = 240,240,240 # lighter grey
+
+# Defaults
+dark  = 0,0,0                  # black
+light = 230, 230, 230          # light grey
+panel_colour = 253,253,253     # off white
+space_colour = 153, 204, 153   # light green
+helpGreen = 100,200,100        # brighter green
+highlight_colour = 240,240,240 # lighter grey
 
 dark_defaults = {'black':black, 'red':red, 'purple':purple}
 light_defaults = {'white':white, 'blue':blue, 'yellow':yellow}
@@ -51,12 +64,12 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 colours = config['colours']
-dark  = colours.get('dark',black)
-light = strToColour(colours['light'])
-space_colour = strToColour(colours['space_colour'])
-helpGreen = strToColour(colours['help_colour'])
-panel_colour = strToColour(colours['panel_colour'])
-highlight_colour = strToColour(colours['highlight_colour'])
+dark  = colourCheck(colours.get('dark'),dark)
+light = colourCheck(colours.get('light'),light)
+space_colour = colourCheck(colours.get('space_colour'),space_colour)
+helpGreen = colourCheck(colours.get('help_colour'),helpGreen)
+panel_colour = colourCheck(colours.get('panel_colour'),panel_colour)
+highlight_colour = colourCheck(colours.get('highlight_colour'),highlight_colour)
 
 window = config['window']
 width = int(window['width'])
